adca44569b27b794090bb182206aa3e87d002d4b
ryan <ryanwkoch@gmail.com>
2017-08-07 16:04:14 -0400

    index.js required productEvent handlers
    required the event handlers:
    const productEvents = require('./products/events')
    
    and then added to doc ready:
    productEvents.addHandlers()
    
    partner BL

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,10 +3,12 @@
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const authEvents = require('./auth/events')
+const productEvents = require('./products/events')
 
 $(() => {
   setAPIOrigin(location, config)
   authEvents.addAuthHandlers()
+  productEvents.addHandlers()
 })
 
 // use require with a reference to bundle the file and use it in this file
