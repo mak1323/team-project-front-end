5fd6ae232f2be7d768fbf79af8332d04b9eb6506
bradleyden <bleyden92@gmail.com>
2017-08-08 10:33:42 -0400

    Implement Add to Cart feature
    Adds a cart variable to products/ui.js that takes in values
    for id and quantity as an array when a user adds an item to their
    cart from the products page.
    
    Also adds a products table to HTML and a products handlebars file
    to properly display products and the add to cart form when the show all
    products button is hit.
    
    Coding partner: Ryan K

--- assets/scripts/products/ui.js
+++ assets/scripts/products/ui.js
@@ -1,9 +1,32 @@
 'use strict'
 // const store = require('../store')
+const showProductsTemplate = require('../templates/products.handlebars')
+
+// This variable represents the array of products that will be patched into the
+// active order.
+const shoppingCart = []
+
+// When a user adds an item to an order, this will pass the item's id and the
+// quanity value the user entered into an array and pushes it to the shopping
+// cart array.
+const onAddItemToOrder = function (event) {
+  console.log(event)
+  event.preventDefault()
+  const item = [$(this).closest('form').find("input[name='id']").val(), $(this).closest('form').find("input[name='quantity']").val()]
+  shoppingCart.push(item)
+  console.log(shoppingCart)
+  console.log()
+}
 
 const showAllProductsSuccess = function (data) {
   console.table(data)
+  const showProductsHTML = showProductsTemplate({ products: data.products })
+  $('#productTable').show()
+  $('#productTable tbody').empty()
+  $('#productTable tbody').append(showProductsHTML)
+  $('.addToCart').on('submit', onAddItemToOrder)
 }
+
 const showAllProductsFailure = function () {
 }
 
