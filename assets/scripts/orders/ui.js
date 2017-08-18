cab6f52f0e3b7df243d5307d05747eb3ba5dc405
mak1323 <myleskaeding@gmail.com>
2017-08-18 14:46:42 -0400

    Fixing orders parsing
    Past orders now takes the specific info out of the orders array,
    and displays only closed orders with parsed date.

--- assets/scripts/orders/ui.js
+++ assets/scripts/orders/ui.js
@@ -3,13 +3,36 @@ const showOrdersTemplate = require('../templates/orders.handlebars')
 const store = require('../store')
 
 const showAllOrdersSuccess = function (data) {
-  console.log(test)
+  console.table(data)
   store.orders = data.orders
-  console.log(store.orders)
-  //filter through here
-  const showOrdersHTML = showOrdersTemplate({ orders: store.orders })
+  const hbsArr = []
+
+  store.orders.forEach(function (order) {
+    if (order.isOpen === false) {
+
+      const pojo = {
+        date_placed: order.date_placed.split('T')[0],
+        products: order.products,
+        total: order.salesProof.amount
+      }
+
+      hbsArr.push(pojo)
+    } else {
+      store.currentOrder = order
+    }
+  })
+  console.log('This is the handlebars stuff', hbsArr)
+  // a function saves current open order to store.openorder
+
+  // a function that takes the data we want from past orders and puts into new array
+
+  // store.orders.forEach(
+  //  {}
+
+  const showOrdersHTML = showOrdersTemplate({ orders: hbsArr })
   $('#previousOrderTable').append(showOrdersHTML)
 }
+
 const showAllOrdersFailure = function () {
 }
 
